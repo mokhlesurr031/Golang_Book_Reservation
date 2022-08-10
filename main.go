3615d6c825f3c156da405bfbf90d1cbe366e0a1e
mokhlesurr031 <mokhlesurr031@gmail.com>
2022-08-11 05:49:27 +0600

    [ADD] Function based http handler error handled

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	_, _ = fmt.Fprintf(w, "This is the home page")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
 	sum := AddValues(2, 5)
 
-	fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2+2=%d", sum))
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2+2=%d", sum))
 }
 
 func AddValues(x, y int) int {
